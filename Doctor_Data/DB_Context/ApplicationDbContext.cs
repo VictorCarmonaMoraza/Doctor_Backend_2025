d8821c06258712a446ef76dc9adafa3bb0ffa507
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-03-06 21:53:20 +0100

    Creamos cadena de conexion y DbContext

---
+++ Doctor_Data/DB_Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Doctor_Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doctor_Data.DB_Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //Con los nombre en plural es como se crearan las tablas en la base de datos
+        public DbSet<User> Users { get; set; }
+    }
+}
