5bfe390a0b51778743d199be4fd67b9333824b4a
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-03-07 21:07:28 +0100

    Migracion de dos nuevas propiedades para almacenar password

--- Doctor_Data/Migrations/20250307200536_agregar_passsword_entidad_usuario.Designer.cs
+++ Doctor_Data/Migrations/20250307200536_agregar_passsword_entidad_usuario.Designer.cs
@@ -3,6 +3,7 @@ using Doctor_Data.DB_Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Doctor_Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250307200536_agregar_passsword_entidad_usuario")]
+    partial class agregar_passsword_entidad_usuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Doctor_Model/Entities/User.cs
+++ Doctor_Model/Entities/User.cs
@@ -4,5 +4,13 @@
     {
         public int Id { get; set; }
         public string? UserName { get; set; }
+        /// <summary>
+        /// Es un arreglo de bytes (byte[]) que almacena la versión encriptada de la contraseña del usuario.
+        /// </summary>
+        public byte[]? PasswordHash { get; set; }
+        /// <summary>
+        /// Un "salto" es una secuencia aleatoria de bytes agregada antes de hacer el hash de la contraseña.
+        /// </summary>
+        public byte[]? PasswordSalt { get; set; }
     }
 }
