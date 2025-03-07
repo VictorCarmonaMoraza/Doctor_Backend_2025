8a157a594ff0516ed08760e80727f80ed8a7df27
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-03-07 08:08:20 +0100

    Agregar un nuevo controlador API

---
+++ API_DOCTOR/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Doctor_Data.DB_Context;
+using Doctor_Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DOCTOR.Controllers
+{
+    [Route("api/[controller]")] // api/user
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+
+        /// <summary>
+        /// Devuelve la lista de usuarios
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<User> GetUsers2()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        /// <summary>
+        /// Devuelve un usuario por su id
+        /// </summary>
+        /// <param name="id">id del usuario</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser2(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ API_DOCTOR/Controllers/UserIActionResultController.cs
@@ -0,0 +1,46 @@
+﻿using Doctor_Data.DB_Context;
+using Doctor_Model.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DOCTOR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserIActionResultController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserIActionResultController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+
+        /// <summary>
+        /// Devuelve la lista de usuarios
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        /// <summary>
+        /// Devuelve un usuario por su id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            return Ok(user);
+        }
+    }
+}
